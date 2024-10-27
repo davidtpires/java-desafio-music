b16e72338f05aac360204cb2051e5e18269c2267
DavidT <davidt.info@gmail.com>
2024-10-27 11:45:31 -0300

    Configurações base, menu, classes e enum

--- src/main/java/br/com/alura/java_desafio_music/model/Artista.java
+++ src/main/java/br/com/alura/java_desafio_music/model/Artista.java
@@ -1,5 +1,97 @@
 package br.com.alura.java_desafio_music.model;
 
-public class Artista {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Artista {
+    private String nome;
+    private TipoArtista tipoArtista;
+    private String generoMusical;
+    private LocalDate dataNascimento;
+    private String nacionalidade;
+    private String biografia;
+
+    private List<Musica> musicas = new ArrayList<>();
+
+    public Artista() {}
+
+    public Artista(String nome, TipoArtista tipoArtista, String generoMusical, LocalDate dataNascimento, String nacionalidade, String biografia, List<Musica> musicas) {
+        this.nome = nome;
+        this.tipoArtista = tipoArtista;
+        this.generoMusical = generoMusical;
+        this.dataNascimento = dataNascimento;
+        this.nacionalidade = nacionalidade;
+        this.biografia = biografia;
+        this.musicas = musicas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public TipoArtista getTipoArtista() {
+        return tipoArtista;
+    }
+
+    public void setTipoArtista(TipoArtista tipoArtista) {
+        this.tipoArtista = tipoArtista;
+    }
+
+    public String getGeneroMusical() {
+        return generoMusical;
+    }
+
+    public void setGeneroMusical(String generoMusical) {
+        this.generoMusical = generoMusical;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public String getBiografia() {
+        return biografia;
+    }
+
+    public void setBiografia(String biografia) {
+        this.biografia = biografia;
+    }
+
+    public List<Musica> getMusicas() {
+        return musicas;
+    }
+
+    public void setMusicas(List<Musica> musicas) {
+        this.musicas = musicas;
+    }
+
+    @Override
+    public String toString() {
+        return "nome='" + nome + '\'' +
+                ", tipoArtista=" + tipoArtista +
+                ", generoMusical='" + generoMusical + '\'' +
+                ", dataNascimento=" + dataNascimento +
+                ", nacionalidade='" + nacionalidade + '\'' +
+                ", biografia='" + biografia + '\'' +
+                ", musicas=" + musicas + '\'';
+    }
 }
+
+
--- src/main/java/br/com/alura/java_desafio_music/model/Musica.java
+++ src/main/java/br/com/alura/java_desafio_music/model/Musica.java
@@ -1,4 +1,82 @@
 package br.com.alura.java_desafio_music.model;
 
+import java.time.LocalDate;
+
 public class Musica {
+
+    private String titulo;
+    private String genero;
+    private int duracao;
+    private LocalDate dataLancamento;
+    private String album;
+    private Artista artista;
+
+    public Musica() {}
+
+    public Musica(String titulo, String genero, int duracao, LocalDate dataLancamento, String album, Artista artista) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.duracao = duracao;
+        this.dataLancamento = dataLancamento;
+        this.album = album;
+        this.artista = artista;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public Artista getArtista() {
+        return artista;
+    }
+
+    public void setArtista(Artista artista) {
+        this.artista = artista;
+    }
+
+    @Override
+    public String toString() {
+        return "titulo='" + titulo + '\'' +
+                ", genero='" + genero + '\'' +
+                ", duracao=" + duracao +
+                ", dataLancamento=" + dataLancamento +
+                ", album='" + album + '\'' +
+                ", artista=" + artista ;
+    }
 }
