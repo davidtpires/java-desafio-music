ef8c5706208b2d18e0a20f54474f09aff024d881
DavidT <davidt.info@gmail.com>
2024-10-27 10:52:37 -0300

    first commit

---
+++ src/main/java/br/com/alura/java_desafio_music/JavaDesafioMusicApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.java_desafio_music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDesafioMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDesafioMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/java_desafio_music/JavaDesafioMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.java_desafio_music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDesafioMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
